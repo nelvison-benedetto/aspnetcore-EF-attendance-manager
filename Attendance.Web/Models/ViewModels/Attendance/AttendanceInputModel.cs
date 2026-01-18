cccb51281a3e7ee7fd3e9e74347946ddb9e2eff2
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-18 16:40:23 +0100

    start

---
+++ Attendance.Web/Models/ViewModels/Attendance/AttendanceInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Attendance.Web.Models.ViewModels.Attendance
+{
+    public class AttendanceInputModel
+    {
+        //rappresenta cio che l'utente invia (fare validazione)
+
+
+    }
+}
