1355ef8e65917aa9a10d9c950996a150885917cd
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-19 23:14:23 +0100

    update

--- Attendance.Web/Controllers/HomeController.cs
+++ Attendance.Web/Controllers/HomeController.cs
@@ -21,24 +21,28 @@ namespace Attendance.Web.Controllers
         public async Task<ActionResult> Index()
         {
             // Ottieni il giorno corrente
-            int todayId = await _dayService.GetOrCreateDayIdAsync(await _dayService.GetTodayAsync());
+            int todayId = await _dayService.GetOrCreateDayIdAsync( await _dayService.GetTodayAsync() );
             // Reindirizza subito alla view del giorno di oggi
-            return RedirectToAction("Day", "Attendance", new { dayId = todayId });
+            return RedirectToAction(
+                "Day",          //actionName (nome del method nel controller di destinazione)
+                "Attendance",   //controllerName (controlle di destinazione ...Controller)
+                new { dayId = todayId }  //routeValues (params passati al method)
+            );
         }
+
 
         // GET: /Home/About
         public ActionResult About()
         {
             ViewBag.Message = "Questa è la web app per la gestione delle presenze.";
-            return View();
+            return View();  
+            //MVC cerca auto view razor /Views/{Controller}/{Action}.cshtml
+            //quindi .../About.cshtml
         }
-
-
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
